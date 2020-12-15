bd8811edade03d836620354e0bee855d137cf119
Suresh Kumar Sunkara <suresh.kumars@live.com>
2020-12-15 22:46:09 +0530

    V2

--- FlashCards.bat
+++ FlashCards.bat
@@ -1,8 +1,3 @@
-cd\
-d:
-
-
-"D:\ProgramData\Anaconda3\envs\FalshCards\python.exe" "D:\Az-900\AzureCert-flash-cards\flash_cards.py" 
-
-"C:\Users\susunkar\AppData\Local\Google\Chrome\Application\chrome.exe" http://127.0.0.1:5000/login
+start microsoft-edge:http://127.0.0.1:5000/login
+"D:\ProgramData\Anaconda3\envs\FalshCards\python.exe" "D:\AZ900-Certification-Preparation\AzureCert-flash-cards\flash_cards.py" 
 
