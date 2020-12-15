bf7f7d354b066d00d7bc7ec59272275a76fbbf5f
Suresh Kumar Sunkara <suresh.kumars@live.com>
2020-12-15 22:18:53 +0530

    V1

---
+++ FlashCards.bat
@@ -0,0 +1,8 @@
+cd\
+d:
+
+
+"D:\ProgramData\Anaconda3\envs\FalshCards\python.exe" "D:\Az-900\AzureCert-flash-cards\flash_cards.py" 
+
+"C:\Users\susunkar\AppData\Local\Google\Chrome\Application\chrome.exe" http://127.0.0.1:5000/login
+
